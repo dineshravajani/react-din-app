fc7429ca1723951c91bb4d7f11111e2d2cc4edf6
Din Turner <dinturner01@gmail.com>
2024-12-18 13:26:20 +0530

    Adding sorting and clear list feature

--- src/TravelList.js
+++ src/TravelList.js
@@ -42,13 +42,29 @@ function Form( {onAddItems}) {
     )
 }
 
-function PackingList( {items, onDeleteItems, onToggleItem }) {
+function PackingList( {items, onDeleteItems, onToggleItem, onClearList }) {
+    const [sortBy, setSortBy] = useState("input");
+
+    let sortedItems;
+
+    if( sortBy === 'input') sortedItems = items;
+    if( sortBy === 'description') sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+    if( sortBy === 'packed') sortedItems = items.slice().sort( (a,b) => Number(a.packed) - Number(b.packed));
     return(
         <div className="list">
             
             <ul>
-                {items.map(item => <Item item={item} key={item.id} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem}/>)}
+                {sortedItems.map(item => <Item item={item} key={item.id} onDeleteItems={onDeleteItems} onToggleItem={onToggleItem}/>)}
             </ul>
+            <div className="actions">
+                <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by Description</option>
+                    <option value="packed">Sort by Packed</option>
+                </select>
+                <button onClick={onClearList}>Clear list</button>
+
+            </div>
         </div>
     )
 }
@@ -91,12 +107,17 @@ function TravelList() {
     function handleToggleItem(id) {
         setItems( items => items.map( item => item.id === id ? { ...item, packed: !item.packed} : item))
     }
+    function handleClearList() {
+        if(window.confirm('Are you sure you want to delete all items ? ')) {
+            setItems([])
+        }
+    }
 
     return(
         <div className="app">
             <Logo/>
             <Form onAddItems={handleAddItems}/>
-            <PackingList items={items} onDeleteItems={handleDeleteItem} onToggleItem={handleToggleItem}/>
+            <PackingList items={items} onDeleteItems={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList}/>
             <Stats items={items}/>
         </div>
     )
